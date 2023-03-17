8c8da69b0d04edc77b80ea0798f6c8b29b165b59
KalMan <118040371+KalMan0@users.noreply.github.com>
2023-03-17 11:13:58 -0700

    Update spammer.py

--- spammer.py
+++ spammer.py
@@ -2,20 +2,24 @@ import requests
 import json
 from time import sleep
 
-webhook_url = 'enter url here'
+webhook_url = 'enter webhook url here'
 message = "enter message here"
-Ammount_of_msg = 5 #change the amount of messages you wish to send 
+Ammount_of_msg = 10 #change the amount of messages you wish to send 
 
 payload= {
     'content': message
+}
+
+headers= {
+    'Content-Type': 'application/json'
 }
 
 data = json.dumps(payload)
 
 def main ():
     for i in range(Ammount_of_msg):
-        response = requests.post(webhook_url, data=data, headers={'Content-Type': 'application/json'})
-        sleep(0.7)
+        response = requests.post(webhook_url, data=data, headers=headers)
+        sleep(.8)
         if response.status_code == 204:
             print(f'Message sent')
         else:
